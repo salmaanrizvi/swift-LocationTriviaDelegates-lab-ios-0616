b2e8767ec5772f6686a1533f9f4e85012f6f77b4
HaarisMuneer <hom9@cornell.edu>
2016-07-13 13:50:52 -0400

    sets up project base

---
+++ Pods/Target Support Files/Pods-SwiftLocationTriviaDelegates/Pods-SwiftLocationTriviaDelegates-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SwiftLocationTriviaDelegatesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftLocationTriviaDelegatesVersionString[];
+
