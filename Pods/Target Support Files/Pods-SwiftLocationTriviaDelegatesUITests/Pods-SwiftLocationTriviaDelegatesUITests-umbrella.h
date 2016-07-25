7638b0721e2eb5c4ff8553a0619ff1fa8516dbe3
HaarisMuneer <hom9@cornell.edu>
2016-07-25 14:34:46 -0400

    fix solution branch

--- Pods/Target Support Files/Pods-SwiftLocationTriviaDelegatesUITests/Pods-SwiftLocationTriviaDelegatesUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SwiftLocationTriviaDelegatesUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SwiftLocationTriviaDelegatesUITestsVersionString[];
-
